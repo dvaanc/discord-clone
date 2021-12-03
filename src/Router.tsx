62bd6fc81a04cf78ea24bac4aa88cb2e922d44b4
dvaanc <dannyc52521@gmail.com>
2021-12-03 19:11:33 +1100

    quick fix for routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
 import App from './components/app/App';
-import { useSelector } from 'react-redux';
-import { RootState } from './redux/store';
 import Login from './components/login/Login';
 
 export default function Router() {
   return (
     <BrowserRouter basename="/discord-clone">
       <Routes>
-      <Route path="/" element={<Navigate replace to="/app" />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={< Login />} />
         <Route path="/app" element={<App />} />
       </Routes>
